b03d0a9fa5f3cfa6fb2d339f74366332f148d17a
Altaay <aaltaay@uncc.edu>
2017-12-08 23:21:54 -0500

    Added a header template

---
+++ Templates/header.php
@@ -0,0 +1,24 @@
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Extracurricular Activities</title>
+        <link rel="icon" href="./images/favicon.png">
+        <link rel="stylesheet" href="main.css">
+    </head>
+
+    <body>
+        <header>
+            <!-- Page Title -->
+            <h1>Extracurricular Activities</h1>
+        <h1>
+            <span class="yellow">
+                Events Manager
+            </span>
+        </h1>
+        <h2>
+            Reveloutionary Event Manager System
+        </h2>
+        
+        </header>
