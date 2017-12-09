3485fee4200a058f8d191b1359d72c57472de82a
Altaay <aaltaay@uncc.edu>
2017-12-08 23:23:18 -0500

    database error page

---
+++ database_error.php
@@ -0,0 +1,15 @@
+
+
+    <div class="container">
+
+<fieldset id="fieldset">
+<legend>
+    Database Error
+</legend>
+        <p>Database Error</p>
+        <p>There was an error connecting to the database.</p>
+        <p>The database must be installed.</p>
+        <p>MySQL must be running.</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+        <p>&nbsp;</p>
+</fieldset>
