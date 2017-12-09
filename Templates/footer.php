27337e8779b51f9f5cf03075e7e082a06a339e1a
Altaay <aaltaay@uncc.edu>
2017-12-08 23:22:08 -0500

    Added a footer template

---
+++ Templates/footer.php
@@ -0,0 +1,9 @@
+            <footer>
+                <p>&copy;
+                    <?php echo date("Y"); ?> University of North Carolina at Charlotte, Charlotte, NC 28262</p>
+                <a href="https://validator.w3.org/check?uri=referer">Validate HTML</a> |
+                <a href="https://jigsaw.w3.org/css-validator/check/referer">Validate CSS</a>
+            </footer>
+        </div>
+    </body>
+    </html>
